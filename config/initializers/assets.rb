d89b8f97da900eb165bcd29af9c74fb636c9bce1
Phillip Malboeuf <phil@phils.computer>
2026-01-14 11:24:31 -0500

    first go at react
    (cherry picked from commit 7c0931ab9ca18eca46837f8c774c9299369a1190)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Note: Propshaft automatically serves files from app/assets/builds/
+# No need to manually add the builds directory to asset paths
