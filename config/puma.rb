5bae77fbb7659b1dc9a28d9c3bb01c14c7ab83a3
Phillip Malboeuf <phil@phils.computer>
2026-01-15 01:46:10 -0500

    Refactor deployment to use Dokku with Let's Encrypt
    - Replace ALB/ACM setup with Dokku Let's Encrypt plugin
    - Point Route 53 directly to EC2 instances
    - Simplify deployment workflow by removing redundant checks
    - Configure Dokku ports for HTTP/HTTPS (80/443 -> 5000)
    - Add automated RDS database creation per branch
    - Update production config for SSL termination at Dokku layer

--- config/puma.rb
+++ config/puma.rb
@@ -28,8 +28,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listen on to receive requests.
+# Dokku sets PORT=5000 by default, but we can use 5000 as fallback
+port ENV.fetch("PORT", 5000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,7 @@
 
 ActiveRecord::Schema[8.1].define(version: 2025_12_29_000000) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-  enable_extension "pscale_extensions.hypopg"
+  enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
